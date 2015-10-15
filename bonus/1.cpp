84f0e8d453cee132847a9dd61a757f006f03068a
Adriana Velkova <adrivelkova@abv.bg>
2015-10-15 03:14:38 +0300

    'asdf'

---
+++ bonus/1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	int a;
+	int b;
+	int result;
+	cout << "a:";
+	cin >> a;
+	cout << "b:";
+	cin >> b;
+	if (a%b==0) {
+		cout << "deli se na b";
+}
+	else {
+		cout << "ne se deli na b";
+}
+	return 0;
+}
